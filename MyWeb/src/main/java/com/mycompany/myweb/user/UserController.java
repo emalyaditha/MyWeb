91d9dbe95ba04433dbc89168d6a085554937a791
Emal Yaditha <93852210+emalyaditha@users.noreply.github.com>
2022-08-16 23:30:00 +0530

    Add files via upload

---
+++ MyWeb/src/main/java/com/mycompany/myweb/user/UserController.java
@@ -0,0 +1,33 @@
+package com.mycompany.myweb.user;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
+
+@Controller
+public class UserController {
+    @Autowired private UserService service;
+
+    @GetMapping("/users")
+    public String showUserList(Model model) {
+        List<User> listUser = service.listAll();
+        model.addAttribute("listUser", listUser);
+
+        return "users";
+    }
+    @GetMapping("users/new")
+
+    public String showNewForm(Model model) {
+        model.addAttribute("user",new User());
+        return "user_form";
+    }
+    @PostMapping("/users/add")
+    public String saveUser (User user) {
+        service.save(user);
+        return "redirect:/users";
+    }
+}
