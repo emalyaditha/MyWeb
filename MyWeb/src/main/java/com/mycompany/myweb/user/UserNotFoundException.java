22ed61f4d1272b9d9a4383eb2bb22d82cb531dcf
Emal Yaditha <93852210+emalyaditha@users.noreply.github.com>
2022-08-18 13:07:50 +0530

    Delete MyWeb directory

--- MyWeb/src/main/java/com/mycompany/myweb/user/UserNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.mycompany.myweb.user;
-
-public class UserNotFoundException extends Throwable {
-    public UserNotFoundException(String message) {
-        super(message);
-    }
-}
--- MyWeb/src/main/java/com/mycompany/myweb/user/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.mycompany.myweb.user;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface UserRepository extends CrudRepository<User, Integer> {
-    public long countById (Integer id);
-}
